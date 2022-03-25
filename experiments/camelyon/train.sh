b23d165d5293bd0edee2c81561b33097fc8da22e
Amir-Hossein Shahidzadeh <c828l050@cmlgrad00.umiacs.umd.edu>
2022-03-25 15:40:44 -0400

    camelyon dataloader added

---
+++ experiments/camelyon/train.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+now=$(date +"%Y%m%d_%H%M%S")
+PARTITION=$1
+JOB_NAME=$2
+ROOT=../..
+export CUDA_VISIBLE_DEVICES=0
+export PYTHONPATH=$ROOT:$PYTHONPATH
+
+
+python -m torch.distributed.launch --nproc_per_node=1 --master_port 29522 ../../train_contrast.py --config=config.yaml  2>&1 | tee log_$now.txt
+mkdir -p checkpoints/result
+#python ../../eval.py --base_size=2048 --scales 1.0 --config=config.yaml --model_path=checkpoints/best.pth --save_folder=checkpoints/result/ 2>&1 | tee checkpoints/result/eva_$now.log
+#python ../../eval.py --base_size=2048 --scales 1.0 --config=config.yaml --model_path=checkpoints/epoch_99.pth --save_folder=checkpoints/result/ 2>&1 | tee checkpoints/result/eva_99_$now.log
+#
