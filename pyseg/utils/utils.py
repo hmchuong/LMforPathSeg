204de35be7dd2061f08ff336427aa78899e5bd41
Chuong Minh Huynh <c828l028@class00.umiacs.umd.edu>
2022-04-03 19:27:20 -0400

    [ADD] dice coeff

--- pyseg/utils/utils.py
+++ pyseg/utils/utils.py
@@ -215,6 +215,44 @@ def precisionAndrecall(output, target, K, ignore_index=255):
     #area_union = area_output + area_target - area_intersection
     return area_intersection, area_output, area_target
 
+def dice(im1, im2, empty_score=1.0):
+    """
+    Computes the Dice coefficient, a measure of set similarity.
+    Parameters
+    ----------
+    im1 : array-like, bool
+        Any array of arbitrary size. If not boolean, will be converted.
+    im2 : array-like, bool
+        Any other array of identical size. If not boolean, will be converted.
+    Returns
+    -------
+    dice : float
+        Dice coefficient as a float on range [0,1].
+        Maximum similarity = 1
+        No similarity = 0
+        Both are empty (sum eq to zero) = empty_score
+        
+    Notes
+    -----
+    The order of inputs for `dice` is irrelevant. The result will be
+    identical if `im1` and `im2` are switched.
+    """
+    im1 = np.squeeze(np.asarray(im1).astype(np.bool))
+    im2 = np.squeeze(np.asarray(im2).astype(np.bool).squeeze())
+    
+    if im1.shape != im2.shape:
+        print(im1.shape)
+        print(im2.shape)
+        raise ValueError("Shape mismatch: im1 and im2 must have the same shape.")
+
+    im_sum = im1.sum() + im2.sum()
+    if im_sum == 0:
+        return empty_score
+
+    # Compute Dice coefficient
+    intersection = np.logical_and(im1, im2)
+
+    return 2. * intersection.sum() / im_sum
 
 def load_trained_model(model, loaded_dict):
     net_state_dict = model.state_dict()
