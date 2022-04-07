0898786714e1eb30e94b8879540a9fa8839e9b1b
Amir-Hossein Shahidzadeh <c828l050@cmlgrad02.umiacs.umd.edu>
2022-04-07 09:23:15 -0400

    smaller cam dataset

--- experiments/camelyon/train_slurm.sh
+++ experiments/camelyon/train_slurm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=train_camelyon16        
-#SBATCH --output=slurm_%A.out
+#SBATCH --output=slurm.out
 #SBATCH --error=slurm_%A.err
 #SBATCH --gres=gpu:1
 #SBATCH --partition=class
@@ -9,7 +9,7 @@
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --mem-per-cpu=4G
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=10
 #SBATCH --time=12:00:00
 
 #module purge
