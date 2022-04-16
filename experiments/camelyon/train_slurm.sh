bca63f1cee4fc460978a542f79ec1659ab1491b7
Chuong Minh Huynh <c828l028@class00.umiacs.umd.edu>
2022-04-15 21:03:23 -0400

    [ADD] config for best training

--- experiments/camelyon/train_slurm.sh
+++ experiments/camelyon/train_slurm.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
-#SBATCH --job-name=train_camelyon16        
-#SBATCH --output=slurm_%A.out
-#SBATCH --error=slurm_%A.err
+#SBATCH --job-name=cam_0.1
+#SBATCH --output=logs/slurm_contrast_unconnected_0.1_%A.out
+#SBATCH --error=logs/slurm_contrast_unconnected_0.1_%A.err
 #SBATCH --gres=gpu:1
 #SBATCH --partition=class
 #SBATCH --account=class
 #SBATCH --qos=default
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
-#SBATCH --mem-per-cpu=4G
+#SBATCH --nodelist=cmlgrad07
+#SBATCH --mem-per-cpu=8G
 #SBATCH --cpus-per-task=4
-#SBATCH --time=12:00:00
+#SBATCH --time=24:00:00
 
-#module purge
-#module load cuda/11.1.1
-#source /fs/classhomes/spring2022/cmsc828l/c828l028/.bashrc
-#conda activate semseg
+module purge
+module load cuda/11.1.1
+source /fs/classhomes/spring2022/cmsc828l/c828l028/.bashrc
+conda activate semseg
 now=$(date +"%Y%m%d_%H%M%S")
 ROOT=../..
 export CUDA_VISIBLE_DEVICES=0
 export PYTHONPATH=$ROOT:$PYTHONPATH
 
 
-python ../../train_contrast.py --config=/fs/classhomes/spring2022/cmsc828l/c828l050/RegionContrast-Med/experiments/camelyon/config_contrast.yaml
+python ../../train_contrast.py --config=config_contrast_unconnected_0.1.yaml
